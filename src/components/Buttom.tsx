ddbe69e675086b6492f1bfcf5540fb663235bdbd
renanlisboa <renlp89.developer@gmail.com>
2022-09-19 22:17:05 -0300

    refactor: button label

--- src/components/Buttom.tsx
+++ src/components/Buttom.tsx
@@ -1,14 +1,21 @@
 import { forwardRef } from "react";
-import { Button as MuiButton, ButtonProps } from "@mui/material";
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from "@mui/material";
+
+export type ButtonProps = MuiButtonProps & {
+  label: string;
+};
 
 export const Button = forwardRef(
   (
-    { variant = "contained", size = "large", children, ...rest }: ButtonProps,
+    { variant = "contained", size = "large", label, ...rest }: ButtonProps,
     ref
   ) => {
     return (
       <MuiButton ref={ref as any} variant={variant} size={size} {...rest}>
-        {children}
+        {label}
       </MuiButton>
     );
   }
